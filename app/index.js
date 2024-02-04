869026c38d934235e050d0a77c29e4fd49cebf58
Christopher Rudy <crudysandiego@Christophers-MacBook-Pro-2.local>
2024-02-04 15:22:29 -0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+
+const Home = () => {
+    const router = useRouter();
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { flex: 1, backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"></ScreenHeaderBtn>
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%"></ScreenHeaderBtn>
+                    ),
+                    headerTitle: ""
+                }}
+            />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{flex: 1, padding: SIZES.medium}}>
+                    <Welcome></Welcome>
+                    <Popularjobs></Popularjobs>
+                    <Nearbyjobs></Nearbyjobs>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default Home;
