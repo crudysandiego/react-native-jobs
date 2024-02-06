cf590cb5209a4b17bf839b301a667a581b4e25de
Christopher Rudy <crudysandiego@Christophers-MacBook-Pro-2.local>
2024-02-05 20:06:33 -0800

    implement nearbyjobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -11,6 +11,13 @@ const Popularjobs = () => {
   const router = useRouter();
   const { data, isLoading, error } = useFetch('search', { query: 'React developer', num_pages: 1 });
 
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -29,7 +36,12 @@ const Popularjobs = () => {
           <FlatList 
             data={ data }
             renderItem={({ item }) => (
-              <PopularJobCard item={item}></PopularJobCard>
+              <PopularJobCard 
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              >
+              </PopularJobCard>
             )}
             keyExtractor={item => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
